77a01ce4dd796644b3a2e3c1032e759175088124
Sibiraj <20282546+sibiraj-s@users.noreply.github.com>
2023-12-09 14:29:41 +0530

    Update ui backgrounds

--- client/app/signup/page.tsx
+++ client/app/signup/page.tsx
@@ -2,12 +2,12 @@ import SignupForm from './SignupForm';
 
 export default function Signup() {
   return (
-    <section className="bg-gray-50 dark:bg-gray-900 h-full">
+    <section className="h-full">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0 h-full">
         <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
           Acme Corp.
         </a>
-        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-950 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
               Create your account
