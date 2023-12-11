05d0882c345ee1b81406717e1df00e4ec98b907a
Sibiraj <20282546+sibiraj-s@users.noreply.github.com>
2023-12-11 13:32:17 +0530

    Format tailwind classes

--- client/app/login/LoginForm.tsx
+++ client/app/login/LoginForm.tsx
@@ -29,13 +29,13 @@ export default function SignupForm() {
   return (
     <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit(handleFormSubmit)}>
       <div>
-        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+        <label htmlFor="email" className="mb-2 block text-sm font-medium text-gray-900 dark:text-white">
           Your email
         </label>
         <input
           type="text"
           id="email"
-          className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          className="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 sm:text-sm"
           placeholder="Enter your email"
           required
           {...register('email')}
@@ -43,14 +43,14 @@ export default function SignupForm() {
       </div>
       <button
         type="submit"
-        className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+        className="bg-primary-600 hover:bg-primary-700 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 w-full rounded-lg px-5 py-2.5 text-center text-sm font-medium text-white focus:outline-none focus:ring-4"
         disabled={formState.isSubmitting}
       >
         Log in
       </button>
       <p className="text-sm font-light text-gray-500 dark:text-gray-400">
         Don&apos;t have an account yet?{' '}
-        <Link href="/signup" className="font-medium text-primary-600 hover:underline dark:text-primary-500">
+        <Link href="/signup" className="text-primary-600 dark:text-primary-500 font-medium hover:underline">
           Sign up
         </Link>
       </p>
--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -3,13 +3,13 @@ import SignupForm from './LoginForm';
 export default function Signup() {
   return (
     <section className="h-full">
-      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0 h-full">
-        <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+      <div className="mx-auto flex h-full flex-col items-center justify-center px-6 py-8 md:h-screen lg:py-0">
+        <a href="#" className="mb-6 flex items-center text-2xl font-semibold text-gray-900 dark:text-white">
           Acme Corp.
         </a>
-        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-950 dark:border-gray-700">
-          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+        <div className="w-full rounded-lg bg-white shadow dark:border dark:border-gray-700 dark:bg-gray-950 sm:max-w-md md:mt-0 xl:p-0">
+          <div className="space-y-4 p-6 sm:p-8 md:space-y-6">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 dark:text-white md:text-2xl">
               Log in your account
             </h1>
             <SignupForm />
