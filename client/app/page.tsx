cce4e9bce1c0c2022aed54607b33bd214afccd2d
Sibiraj <20282546+sibiraj-s@users.noreply.github.com>
2023-12-11 14:19:15 +0530

    Normalize forms and cleanup UI

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -4,13 +4,13 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Signup and Login powered by&nbsp;
           <Link href="https://github.com/go-webauthn/webauthn" target="_blank" className="hover:underline">
             <code className="font-mono font-bold">go-webauthn</code>
           </Link>
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center lg:static lg:h-auto lg:w-auto lg:bg-none">
           <Link
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
             href="https://github.com/sibiraj-s"
@@ -22,8 +22,8 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="before:bg-gradient-radial after:bg-gradient-conic relative z-[-1] flex h-36 place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <span className="text-center text-5xl">WebAuthn Spec Demo</span>
+      <div className="flex h-36 place-items-center">
+        <span className="text-center text-4xl md:text-6xl">WebAuthn Spec Demo</span>
       </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
@@ -32,12 +32,12 @@ export default function Home() {
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Sign up{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
+            Sign up
+            <span className="ml-2 inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              {'->'}
             </span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">
             Effortless Registration, Fortified Security with WebAuthn.
           </p>
         </Link>
@@ -47,12 +47,12 @@ export default function Home() {
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Login{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
+            Login
+            <span className="ml-2 inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              {'->'}
             </span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Unparalleled Security - Login the WebAuthn Way.</p>
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">Unparalleled Security - Login the WebAuthn Way.</p>
         </Link>
       </div>
     </main>
--- client/app/signup/SignupForm.tsx
+++ client/app/signup/SignupForm.tsx
@@ -35,7 +35,7 @@ export default function SignupForm() {
         <input
           type="email"
           id="email"
-          className="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 sm:text-sm"
+          className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 focus:border-indigo-600 focus:ring-indigo-600 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 sm:text-sm"
           placeholder="Enter your email"
           required
           {...register('email')}
@@ -43,14 +43,14 @@ export default function SignupForm() {
       </div>
       <button
         type="submit"
-        className="bg-primary-600 hover:bg-primary-700 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 w-full rounded-lg px-5 py-2.5 text-center text-sm font-medium text-white focus:outline-none focus:ring-4"
+        className="w-full rounded-lg bg-indigo-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-4 focus:ring-indigo-300 dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-800"
         disabled={formState.isSubmitting}
       >
         Sign up
       </button>
       <p className="text-sm font-light text-gray-500 dark:text-gray-400">
         Already having an account?{' '}
-        <Link href="/login" className="text-primary-600 dark:text-primary-500 font-medium hover:underline">
+        <Link href="/login" className="font-medium text-indigo-600 hover:underline dark:text-indigo-500">
           Log in
         </Link>
       </p>
